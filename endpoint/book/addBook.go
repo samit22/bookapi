2edabae622eacd801f0f23f917ddcf907dd2d9a5
samit22 <samit@cloudfactory.com>
2019-09-03 23:55:31 +0545

    Add validation.

--- endpoint/book/addBook.go
+++ endpoint/book/addBook.go
@@ -3,21 +3,22 @@ package book
 import (
 	"context"
 
+	"github.com/graniticio/granitic/v2/types"
 	"github.com/graniticio/granitic/v2/ws"
 )
 
 type AddBookLogic struct{}
 
 type AddBookRequest struct {
-	Name   string `json:"name"`
-	Author string `json:author`
+	Name   *types.NilableString `json:"name"`
+	Author *types.NilableString `json:"author"`
 }
 
 func (gl *AddBookLogic) ProcessPayload(ctx context.Context, req *ws.Request, res *ws.Response, cb *AddBookRequest) {
 
 	a := new(Book)
-	a.Name = cb.Name
-	a.Author = cb.Author
+	a.Name = cb.Name.String()
+	a.Author = cb.Author.String()
 
 	res.Body = a
 	res.HTTPStatus = 201
