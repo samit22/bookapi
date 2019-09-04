f3bffeb534dd55e9bb8c8ded41b3a3d554a9b2d3
samit22 <samit@cloudfactory.com>
2019-09-04 13:27:01 +0545

    Add file writer.

--- endpoint/book/addBook.go
+++ endpoint/book/addBook.go
@@ -3,23 +3,33 @@ package book
 import (
 	"context"
 
+	"github.com/graniticio/granitic/v2/logging"
 	"github.com/graniticio/granitic/v2/types"
 	"github.com/graniticio/granitic/v2/ws"
 )
 
-type AddBookLogic struct{}
+type AddBookLogic struct {
+	Log         logging.Logger
+	FileManager FileAccessor
+}
+
+type FileAccessor interface {
+	Write(name string, auth string) error
+}
 
 type AddBookRequest struct {
 	Name   *types.NilableString `json:"name"`
 	Author *types.NilableString `json:"author"`
 }
 
-func (gl *AddBookLogic) ProcessPayload(ctx context.Context, req *ws.Request, res *ws.Response, cb *AddBookRequest) {
+func (al *AddBookLogic) ProcessPayload(ctx context.Context, req *ws.Request, res *ws.Response, cb *AddBookRequest) {
 
-	a := new(Book)
-	a.Name = cb.Name.String()
-	a.Author = cb.Author.String()
-
-	res.Body = a
+	err := al.FileManager.Write(cb.Name.String(), cb.Author.String())
+	if err != nil {
+		al.Log.LogErrorf("Could not open file: %v", err)
+		res.HTTPStatus = 400
+		res.Body = map[string]string{"message": "Something went wrong"}
+	}
 	res.HTTPStatus = 201
+	res.Body = map[string]string{"message": "Successfully added"}
 }
